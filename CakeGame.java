5954b50f4ee41166aec5f52df161dee8f1f0da35
bhairav <bhairavraavan@gmail.com>
2024-11-30 11:11:15 -0500

    initial commit

---
+++ CakeGame.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CakeGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count1 = 0;
+        int count2 = 0;
+
+        System.out.println("Input either 1 or 2 that decides which player will eat the cake");
+
+        for (int i = 0; i < 5; i++) {
+            int flip = scanner.nextInt();
+            if (flip == 1) {
+                count1 += 1;
+            } else {
+                count2 += 1;
+            }
+        }
+
+        if (count1 > count2) {
+            System.out.println("Player1 eats the cake.");
+        } else {
+            System.out.println("Player2 eats the cake.");
+        }
+
+        scanner.close();
+    }
+}
