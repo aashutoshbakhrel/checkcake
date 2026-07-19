525130e3ed6a45724b5bb2e2b46e2e770b9da900
Aashutosh Bakhrel <74250907+aashutoshbakhrel@users.noreply.github.com>
2026-07-19 14:35:42 +0545

    Update CakeGame.java
    July 19 2026

--- CakeGame.java
+++ CakeGame.java
@@ -6,21 +6,31 @@ public class CakeGame {
         int count1 = 0;
         int count2 = 0;
 
-        System.out.println("EDITED: 2.0 Input either 1 or 2 that decides which player will eat the cake");
+        System.out.println("EDITED: 3.0");
+        System.out.println("Enter either 1 or 2 (5 valid inputs required):");
 
-        for (int i = 0; i < 5; i++) {
+        int validInputs = 0;
+
+        while (validInputs < 5) {
             int flip = scanner.nextInt();
+
             if (flip == 1) {
-                count1 += 1;
+                count1++;
+                validInputs++;
+            } else if (flip == 2) {
+                count2++;
+                validInputs++;
             } else {
-                count2 += 1;
+                System.out.println("Invalid input! Please enter only 1 or 2.");
             }
         }
 
         if (count1 > count2) {
             System.out.println("Player1 eats the cake.");
-        } else {
+        } else if (count2 > count1) {
             System.out.println("Player2 eats the cake.");
+        } else {
+            System.out.println("It's a tie! Nobody eats the cake.");
         }
 
         scanner.close();
